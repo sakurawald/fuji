128b45442dfc5d02d0dbfc2df84a9232d67c5443
SakuraWald <sakurawald@gmail.com>
2023-09-07 16:46:03 +0800

    ADD op protect

---
+++ src/main/java/fun/sakurawald/mixin/op_protect/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,25 @@
+package fun.sakurawald.mixin.op_protect;
+
+
+import fun.sakurawald.ModMain;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class ServerPlayNetworkHandlerMixin {
+
+    @Shadow
+    public ServerPlayerEntity player;
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;onDisconnect()V"), method = "onDisconnected")
+    private void onPlayerLeave(Text reason, CallbackInfo info) {
+        ModMain.SERVER.getPlayerManager().removeFromOperators(player.getGameProfile());
+    }
+
+}
