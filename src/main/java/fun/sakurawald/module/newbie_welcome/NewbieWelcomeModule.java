f5e9fa769de57307d3b6d702407b303fddd9594e
SakuraWald <sakurawald@gmail.com>
2023-09-09 05:19:45 +0800

    FIX NewbieWelcomeModule ignore carpet fake-player

--- src/main/java/fun/sakurawald/module/newbie_welcome/NewbieWelcomeModule.java
+++ src/main/java/fun/sakurawald/module/newbie_welcome/NewbieWelcomeModule.java
@@ -1,11 +1,18 @@
 package fun.sakurawald.module.newbie_welcome;
 
+import fun.sakurawald.ModMain;
 import fun.sakurawald.config.ConfigManager;
 import fun.sakurawald.util.MessageUtil;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class NewbieWelcomeModule {
     public static void welcomeNewbiePlayer(ServerPlayerEntity player) {
+        /* ignore carpet fake-player */
+        if (!ModMain.SERVER.getPlayerManager().isWhitelisted(player.getGameProfile())) {
+            ModMain.LOGGER.info("NewbieWelcomeModule: " + player.getGameProfile() + " is not whitelisted, ignore it.");
+            return;
+        }
+
         /* send welcome message */
         MessageUtil.broadcast(MessageUtil.resolve(ConfigManager.configWrapper.instance().modules.newbie_welcome.welcome_message, player), false);
 
