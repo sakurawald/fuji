a69fef5035a699aa786b5db3accfc30962c10e14
SakuraWald <sakurawald@gmail.com>
2023-09-16 09:27:46 +0800

    ADD /tpa command

--- src/main/java/fun/sakurawald/module/tpa/TpaModule.java
+++ src/main/java/fun/sakurawald/module/tpa/TpaModule.java
@@ -4,20 +4,29 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static net.minecraft.commands.Commands.argument;
 
+@Slf4j
 public class TpaModule {
+
+    @Getter
     private static final ArrayList<TpaRequest> requests = new ArrayList<>();
 
-    public static void registerCommand(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext commandBuildContext, Commands.CommandSelection commandSelection) {
+    public static void registerCommand(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext registryAccess, Commands.CommandSelection environment) {
         dispatcher.register(
                 Commands.literal("tpa").then(argument("player", EntityArgument.player()).executes(TpaModule::$tpa))
         );
@@ -31,7 +40,7 @@ public class TpaModule {
                 Commands.literal("tpadeny").then(argument("player", EntityArgument.player()).executes(TpaModule::$tpadeny))
         );
         dispatcher.register(
-                Commands.literal("tpadeny").then(argument("player", EntityArgument.player()).executes(TpaModule::$tpacancel))
+                Commands.literal("tpacancel").then(argument("player", EntityArgument.player()).executes(TpaModule::$tpacancel))
         );
     }
 
@@ -44,34 +53,96 @@ public class TpaModule {
     }
 
     private static int $tpaaccept(CommandContext<CommandSourceStack> ctx) {
-
-
-        return 0;
+        return doResponse(ctx, ResponseStatus.ACCEPT);
     }
 
     private static int $tpadeny(CommandContext<CommandSourceStack> ctx) {
-        return 0;
+        return doResponse(ctx, ResponseStatus.DENY);
     }
 
     private static int $tpacancel(CommandContext<CommandSourceStack> ctx) {
-
-        return 0;
+        return doResponse(ctx, ResponseStatus.CANCEL);
     }
 
-    private static int doRequest(CommandContext<CommandSourceStack> ctx, boolean tpahere) {
+    @SuppressWarnings("SameReturnValue")
+    private static int doResponse(CommandContext<CommandSourceStack> ctx, ResponseStatus status) {
         ServerPlayer source = ctx.getSource().getPlayer();
+        if (source == null) return Command.SINGLE_SUCCESS;
+
         ServerPlayer player;
         try {
             player = EntityArgument.getPlayer(ctx, "player");
         } catch (CommandSyntaxException e) {
-            throw new RuntimeException(e);
+            source.sendActionBar(Component.text("Player not found", NamedTextColor.RED));
+            return Command.SINGLE_SUCCESS;
         }
 
-        TpaRequest request = new TpaRequest(source, player, false);
+        /* resolve relative request */
+        Optional<TpaRequest> requestOptional = requests.stream()
+                .filter(request ->
+                        status == ResponseStatus.CANCEL ?
+                                (request.getSender().equals(source) && request.getReceiver().equals(player))
+                                : (request.getSender().equals(player) && request.getReceiver().equals(source)))
+                .findFirst();
+        if (requestOptional.isEmpty()) {
+            source.sendActionBar(Component.text("No relative request.", NamedTextColor.RED));
+            return Command.SINGLE_SUCCESS;
+        }
+
+        TpaRequest request = requestOptional.get();
+        if (status == ResponseStatus.ACCEPT) {
+            request.getSender().sendActionBar(request.asSenderComponent$Accepted());
+            request.getReceiver().sendActionBar(request.asReceiverComponent$Accepted());
+            ServerPlayer who = request.getTeleportWho();
+            ServerPlayer to = request.getTeleportTo();
+            who.teleportTo((ServerLevel) to.level(), to.getX(), to.getY(), to.getZ(), to.getYRot(), to.getXRot());
+        } else if (status == ResponseStatus.DENY) {
+            request.getSender().sendActionBar(request.asSenderComponent$Denied());
+            request.getReceiver().sendActionBar(request.asReceiverComponent$Denied());
+        } else if (status == ResponseStatus.CANCEL) {
+            request.getSender().sendActionBar(request.asSenderComponent$Cancelled());
+            request.getReceiver().sendActionBar(request.asReceiverComponent$Cancelled());
+        }
+
+        request.cancelTimeout();
+        requests.remove(request);
+        return Command.SINGLE_SUCCESS;
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    private static int doRequest(CommandContext<CommandSourceStack> ctx, boolean tpahere) {
+        ServerPlayer source = ctx.getSource().getPlayer();
+        if (source == null) return Command.SINGLE_SUCCESS;
+        ServerPlayer player;
+        try {
+            player = EntityArgument.getPlayer(ctx, "player");
+        } catch (CommandSyntaxException e) {
+            source.sendActionBar(Component.text("Player not found", NamedTextColor.RED));
+            return Command.SINGLE_SUCCESS;
+        }
+
+        /* add request */
+        TpaRequest request = new TpaRequest(source, player, tpahere);
+
+        /* has similar request ? */
+        if (request.getSender().equals(request.getReceiver())) {
+            request.getSender().sendActionBar(Component.text("You can't send a request to yourself.", NamedTextColor.RED));
+            return Command.SINGLE_SUCCESS;
+        }
+
+        if (requests.stream().anyMatch(request::similarTo)) {
+            request.getSender().sendActionBar(Component.text("A similar request already exists.", NamedTextColor.RED));
+            return Command.SINGLE_SUCCESS;
+        }
+
+        request.startTimeout();
         requests.add(request);
 
+        /* feedback */
         request.getReceiver().sendMessage(request.asReceiverComponent$Sent());
         request.getSender().sendMessage(request.asSenderComponent$Sent());
         return Command.SINGLE_SUCCESS;
     }
+
+    private enum ResponseStatus {ACCEPT, DENY, CANCEL}
 }
--- src/main/java/fun/sakurawald/module/tpa/TpaRequest.java
+++ src/main/java/fun/sakurawald/module/tpa/TpaRequest.java
@@ -1,95 +1,121 @@
 package fun.sakurawald.module.tpa;
 
 import fun.sakurawald.config.ConfigManager;
+import lombok.Getter;
+import lombok.ToString;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minecraft.server.level.ServerPlayer;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+@ToString
 public class TpaRequest {
 
-    final ServerPlayer teleportWho;
-    final ServerPlayer teleportTo;
-    final boolean tpahere;
+    private static final String TICK = "[✔]";
+    private static final String CROSS = "[❌]";
+    @Getter
+    private final ServerPlayer sender;
+    @Getter
+    private final ServerPlayer receiver;
+    private final boolean tpahere;
     private Timer timer;
 
-    public TpaRequest(ServerPlayer teleportWho, ServerPlayer teleportTo, boolean tpahere) {
-        this.teleportWho = teleportWho;
-        this.teleportTo = teleportTo;
+    public TpaRequest(ServerPlayer sender, ServerPlayer receiver, boolean tpahere) {
+        this.sender = sender;
+        this.receiver = receiver;
         this.tpahere = tpahere;
     }
 
-
-    public ServerPlayer getSender() {
-        return tpahere ? teleportTo : teleportWho;
+    boolean similarTo(TpaRequest other) {
+        return (this.sender.equals(other.sender) && this.receiver.equals(other.receiver)) ||
+                (this.sender.equals(other.receiver) && this.receiver.equals(other.sender));
     }
 
-    public ServerPlayer getReceiver() {
-        return tpahere ? teleportWho : teleportTo;
+    public ServerPlayer getTeleportWho() {
+        return tpahere ? getReceiver() : getSender();
     }
 
-    public Component asSenderComponent$Sent() {
-        return Component.text("You've sent a request to %s".formatted(getReceiver().getGameProfile().getName()));
+    public ServerPlayer getTeleportTo() {
+        return tpahere ? getSender() : getReceiver();
     }
 
-
-    public Component asSenderComponent$Accept() {
-        return Component.text("You've sent a request to %s".formatted(getReceiver().getGameProfile().getName()));
-    }
-
-    public Component asReceiverComponent$Accept() {
-        return Component.text("You've sent a request to %s".formatted(getReceiver().getGameProfile().getName()));
-    }
-
-    public Component asSenderComponent$Deny() {
-        return Component.text("You've sent a request to %s".formatted(getReceiver().getGameProfile().getName()));
-    }
-
-    public Component asReceiverComponent$Deny() {
-        return Component.text("You've sent a request to %s".formatted(getReceiver().getGameProfile().getName()));
-    }
-
-    public Component asSenderComponent$Cancel() {
-        return Component.text("You've sent a request to %s".formatted(getReceiver().getGameProfile().getName()));
-    }
-
-    public Component asReceiverComponent$Cancel() {
-        return Component.text("You've sent a request to %s".formatted(getReceiver().getGameProfile().getName()));
-    }
-
-    public Component asReceiverComponent$Sent() {
-        String who = teleportWho.getName().getString();
-        String to = teleportTo.getName().getString();
-        Component description = tpahere ? Component.text("Teleport you to %s".formatted(who)) : Component.text("Teleport %s to you".formatted(who));
-        Component ret = Component.empty()
-                .append(description)
-                .appendNewline()
-                .append(Component.text("[Accept]").clickEvent(ClickEvent.runCommand("/tpaaccept %s".formatted(tpahere ? to : who))))
-                .appendSpace()
-                .append(Component.text("[Deny]").clickEvent(ClickEvent.runCommand("/tpadeny %s".formatted(tpahere ? to : who))));
-        return ret;
-    }
-
-    void startTimeout(Runnable onTimeout) {
+    public void startTimeout() {
+        var that = this;
         timer = new Timer();
         timer.schedule(
                 new TimerTask() {
                     @Override
                     public void run() {
-                        getSender().sendMessage(asReceiverComponent$Cancel());
-                        getReceiver().sendMessage(asReceiverComponent$Cancel());
-                        onTimeout.run();
+                        getSender().sendMessage(asSenderComponent$Cancelled());
+                        getReceiver().sendMessage(asReceiverComponent$Cancelled());
+                        // don't forget to remove this request
+                        TpaModule.getRequests().remove(that);
                     }
                 },
                 ConfigManager.configWrapper.instance().modules.tpa.timeout * 1000L
         );
     }
 
-    void cancelTimeout() {
+    public void cancelTimeout() {
         timer.cancel();
     }
 
 
+    public Component asSenderComponent$Sent() {
+        TextComponent cancelComponent = Component
+                .text(CROSS).color(NamedTextColor.RED)
+                .hoverEvent(HoverEvent.showText(Component.text("Cancel", NamedTextColor.RED)))
+                .clickEvent(ClickEvent.runCommand("/tpacancel %s".formatted(getReceiver().getGameProfile().getName())));
+
+        return Component.text("Sent a request to %s".formatted(getReceiver().getGameProfile().getName())).color(NamedTextColor.YELLOW)
+                .appendSpace()
+                .append(cancelComponent);
+    }
+
+    public Component asReceiverComponent$Sent() {
+        String sender = this.sender.getName().getString();
+        Component description = (tpahere ? Component
+                .text("You --> %s".formatted(sender)) : Component.text("%s --> You".formatted(sender)))
+                .color(NamedTextColor.YELLOW);
+        Component ret = Component.empty()
+                .append(description
+                        .appendSpace()
+                        .append(Component.text(TICK).color(NamedTextColor.GREEN)
+                                .hoverEvent(HoverEvent.showText(Component.text("Accept this request", NamedTextColor.GREEN)))
+                                .clickEvent(ClickEvent.runCommand("/tpaaccept %s".formatted(sender))))
+                        .appendSpace()
+                        .append(Component.text(CROSS).color(NamedTextColor.RED)
+                                .hoverEvent(HoverEvent.showText(Component.text("Deny this request", NamedTextColor.RED))))
+                        .clickEvent(ClickEvent.runCommand("/tpadeny %s".formatted(sender))));
+        return ret;
+    }
+
+    public Component asSenderComponent$Accepted() {
+        return Component.text("%s has accepted your request".formatted(getReceiver().getGameProfile().getName()), NamedTextColor.YELLOW);
+    }
+
+    public Component asReceiverComponent$Accepted() {
+        return Component.text("Accepted the request from %s".formatted(getSender().getGameProfile().getName()), NamedTextColor.YELLOW);
+    }
+
+    public Component asSenderComponent$Denied() {
+        return Component.text("%s has denied your request".formatted(getReceiver().getGameProfile().getName()), NamedTextColor.YELLOW);
+    }
+
+    public Component asReceiverComponent$Denied() {
+        return Component.text("Denied the request from %s".formatted(getSender().getGameProfile().getName()), NamedTextColor.YELLOW);
+    }
+
+    public Component asSenderComponent$Cancelled() {
+        return Component.text("Cancelled the request to %s".formatted(getReceiver().getGameProfile().getName()), NamedTextColor.YELLOW);
+    }
+
+    public Component asReceiverComponent$Cancelled() {
+        return Component.text("Request from %s has been canceled".formatted(getSender().getGameProfile().getName()), NamedTextColor.YELLOW);
+    }
 }
