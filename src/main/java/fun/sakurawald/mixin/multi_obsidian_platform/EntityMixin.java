4b97cecd559db345c25f9d9ccf635b295aa94a4c
SakuraWald <sakurawald@gmail.com>
2023-09-25 22:09:28 +0800

    fix: resource world ender portal frame will also create its obsidian platform

--- src/main/java/fun/sakurawald/mixin/multi_obsidian_platform/EntityMixin.java
+++ src/main/java/fun/sakurawald/mixin/multi_obsidian_platform/EntityMixin.java
@@ -4,6 +4,7 @@ import fun.sakurawald.module.multi_obsidian_platform.MultiObsidianPlatformModule
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,13 +18,21 @@ public abstract class EntityMixin {
         return MultiObsidianPlatformModule.transform(entity.blockPosition());
     }
 
+    @Unique
+    Level getServerLevel() {
+        Entity entity = (Entity) (Object) this;
+        return entity.level();
+    }
+
     @Redirect(method = "findDimensionEntryPoint", at = @At(value = "FIELD", target = "Lnet/minecraft/server/level/ServerLevel;END_SPAWN_POINT:Lnet/minecraft/core/BlockPos;"))
     BlockPos $findDimensionEntryPoint() {
+        if (getServerLevel().dimension() != Level.OVERWORLD) return ServerLevel.END_SPAWN_POINT;
         return getTransformedEndSpawnPoint();
     }
 
     @Redirect(method = "changeDimension", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;makeObsidianPlatform(Lnet/minecraft/server/level/ServerLevel;)V"))
     public void $changeDimension(ServerLevel serverLevel) {
+        if (serverLevel.dimension() != Level.OVERWORLD) return;
         MultiObsidianPlatformModule.makeObsidianPlatform(serverLevel, getTransformedEndSpawnPoint());
     }
 }
