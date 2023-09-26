02161b12984d4772623bf8deb095a8e80bc053dc
SakuraWald <sakurawald@gmail.com>
2023-09-27 05:38:03 +0800

    fix: multi obsidiab platform

--- src/main/java/fun/sakurawald/module/multi_obsidian_platform/MultiObsidianPlatformModule.java
+++ src/main/java/fun/sakurawald/module/multi_obsidian_platform/MultiObsidianPlatformModule.java
@@ -1,19 +1,47 @@
 package fun.sakurawald.module.multi_obsidian_platform;
 
+import fun.sakurawald.ServerMain;
 import lombok.extern.slf4j.Slf4j;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Blocks;
 
 @Slf4j
-
 public class MultiObsidianPlatformModule {
 
+    private static BlockPos findCenterEndPortalBlock(BlockPos bp) {
+        ServerLevel overworld = ServerMain.SERVER.overworld();
+        if (overworld.getBlockState(bp.north()) != Blocks.END_PORTAL.defaultBlockState() && overworld.getBlockState(bp.west()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.south().east();
+        }
+        if (overworld.getBlockState(bp.north()) != Blocks.END_PORTAL.defaultBlockState() && overworld.getBlockState(bp.east()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.south().west();
+        }
+        if (overworld.getBlockState(bp.south()) != Blocks.END_PORTAL.defaultBlockState() && overworld.getBlockState(bp.west()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.north().east();
+        }
+        if (overworld.getBlockState(bp.south()) != Blocks.END_PORTAL.defaultBlockState() && overworld.getBlockState(bp.east()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.north().west();
+        }
+        if (overworld.getBlockState(bp.north()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.south();
+        }
+        if (overworld.getBlockState(bp.south()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.north();
+        }
+        if (overworld.getBlockState(bp.west()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.east();
+        }
+        if (overworld.getBlockState(bp.east()) != Blocks.END_PORTAL.defaultBlockState()) {
+            return bp.west();
+        }
+        return bp;
+    }
+
     public static BlockPos transform(BlockPos bp) {
-        int xDeviation = bp.getX() > 0 ? 2 : -2;
-        int yDeviation = bp.getY() > 0 ? 2 : -2;
+        bp = findCenterEndPortalBlock(bp);
         int factor = 5;
-        return new BlockPos((bp.getX() + xDeviation) / factor, 50, (bp.getZ() + yDeviation) / factor);
+        return new BlockPos(bp.getX() / factor, 50, bp.getZ() / factor);
     }
 
     public static void makeObsidianPlatform(ServerLevel serverLevel, BlockPos centerBlockPos) {
