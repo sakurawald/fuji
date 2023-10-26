15605e2dc7252982790c90d600b70df1340d487f
SakuraWald <sakurawald@gmail.com>
2023-10-27 07:24:40 +0800

    fix: /afk broadcast missing

--- src/main/java/io/github/sakurawald/mixin/afk/ServerPlayerMixin.java
+++ src/main/java/io/github/sakurawald/mixin/afk/ServerPlayerMixin.java
@@ -63,7 +63,7 @@ public abstract class ServerPlayerMixin implements ServerPlayerAccessor_afk {
     public void sakurawald$setAfk(boolean flag) {
         this.afk = flag;
         this.server.getPlayerList().broadcastAll(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.UPDATE_DISPLAY_NAME, (ServerPlayer) (Object) this));
-        MessageUtil.sendMessage(player, this.afk ? "afk.on" : "afk.off");
+        MessageUtil.sendBroadcast(this.afk ? "afk.on.broadcast" : "afk.off.broadcast", this.player.getGameProfile().getName());
     }
 
     @Override
