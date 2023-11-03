fa890339a906aebe6305996cba60da73d40bf9b3
SakuraWald <sakurawald@gmail.com>
2023-11-03 17:58:31 +0800

    refactor: extract playerOnlyCommand

--- src/main/java/io/github/sakurawald/module/initializer/tpa/TpaModule.java
+++ src/main/java/io/github/sakurawald/module/initializer/tpa/TpaModule.java
@@ -6,6 +6,7 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import io.github.sakurawald.module.initializer.chat.mention.MentionPlayersJob;
+import io.github.sakurawald.util.CommandUtil;
 import io.github.sakurawald.util.MessageUtil;
 import lombok.Getter;
 import net.minecraft.commands.CommandBuildContext;
@@ -70,88 +71,86 @@ public class TpaModule extends ModuleInitializer {
 
     @SuppressWarnings("SameReturnValue")
     private int doResponse(CommandContext<CommandSourceStack> ctx, ResponseStatus status) {
-        ServerPlayer source = ctx.getSource().getPlayer();
-        if (source == null) return Command.SINGLE_SUCCESS;
+        return CommandUtil.playerOnlyCommand(ctx, source -> {
+            ServerPlayer target;
+            try {
+                target = EntityArgument.getPlayer(ctx, "player");
+            } catch (CommandSyntaxException e) {
+                MessageUtil.sendActionBar(source, "tpa.player_not_found");
+                return Command.SINGLE_SUCCESS;
+            }
 
-        ServerPlayer player;
-        try {
-            player = EntityArgument.getPlayer(ctx, "player");
-        } catch (CommandSyntaxException e) {
-            MessageUtil.sendActionBar(source, "tpa.player_not_found");
+            /* resolve relative request */
+            Optional<TpaRequest> requestOptional = requests.stream()
+                    .filter(request ->
+                            status == ResponseStatus.CANCEL ?
+                                    (request.getSender().equals(source) && request.getReceiver().equals(target))
+                                    : (request.getSender().equals(target) && request.getReceiver().equals(source)))
+                    .findFirst();
+            if (requestOptional.isEmpty()) {
+                MessageUtil.sendActionBar(source, "tpa.no_relative_ticket");
+                return Command.SINGLE_SUCCESS;
+            }
+
+            TpaRequest request = requestOptional.get();
+            if (status == ResponseStatus.ACCEPT) {
+                request.getSender().sendActionBar(request.asSenderComponent$Accepted());
+                request.getReceiver().sendMessage(request.asReceiverComponent$Accepted());
+
+                ServerPlayer who = request.getTeleportWho();
+                ServerPlayer to = request.getTeleportTo();
+                MentionPlayersJob.scheduleJob(request.isTpahere() ? to : who);
+                who.teleportTo((ServerLevel) to.level(), to.getX(), to.getY(), to.getZ(), to.getYRot(), to.getXRot());
+            } else if (status == ResponseStatus.DENY) {
+                request.getSender().sendActionBar(request.asSenderComponent$Denied());
+                request.getReceiver().sendMessage(request.asReceiverComponent$Denied());
+            } else if (status == ResponseStatus.CANCEL) {
+                request.getSender().sendMessage(request.asSenderComponent$Cancelled());
+                request.getReceiver().sendMessage(request.asReceiverComponent$Cancelled());
+            }
+
+            request.cancelTimeout();
+            requests.remove(request);
             return Command.SINGLE_SUCCESS;
-        }
-
-        /* resolve relative request */
-        Optional<TpaRequest> requestOptional = requests.stream()
-                .filter(request ->
-                        status == ResponseStatus.CANCEL ?
-                                (request.getSender().equals(source) && request.getReceiver().equals(player))
-                                : (request.getSender().equals(player) && request.getReceiver().equals(source)))
-                .findFirst();
-        if (requestOptional.isEmpty()) {
-            MessageUtil.sendActionBar(source, "tpa.no_relative_ticket");
-            return Command.SINGLE_SUCCESS;
-        }
-
-        TpaRequest request = requestOptional.get();
-        if (status == ResponseStatus.ACCEPT) {
-            request.getSender().sendActionBar(request.asSenderComponent$Accepted());
-            request.getReceiver().sendMessage(request.asReceiverComponent$Accepted());
-
-            ServerPlayer who = request.getTeleportWho();
-            ServerPlayer to = request.getTeleportTo();
-            MentionPlayersJob.scheduleJob(request.isTpahere() ? to : who);
-            who.teleportTo((ServerLevel) to.level(), to.getX(), to.getY(), to.getZ(), to.getYRot(), to.getXRot());
-        } else if (status == ResponseStatus.DENY) {
-            request.getSender().sendActionBar(request.asSenderComponent$Denied());
-            request.getReceiver().sendMessage(request.asReceiverComponent$Denied());
-        } else if (status == ResponseStatus.CANCEL) {
-            request.getSender().sendMessage(request.asSenderComponent$Cancelled());
-            request.getReceiver().sendMessage(request.asReceiverComponent$Cancelled());
-        }
-
-        request.cancelTimeout();
-        requests.remove(request);
-        return Command.SINGLE_SUCCESS;
+        });
     }
 
     @SuppressWarnings("SameReturnValue")
     private int doRequest(CommandContext<CommandSourceStack> ctx, boolean tpahere) {
-        ServerPlayer source = ctx.getSource().getPlayer();
-        if (source == null) return Command.SINGLE_SUCCESS;
-        ServerPlayer player;
-        try {
-            player = EntityArgument.getPlayer(ctx, "player");
-        } catch (CommandSyntaxException e) {
-            MessageUtil.sendActionBar(source, "tpa.player_not_found");
+        return CommandUtil.playerOnlyCommand(ctx, source -> {
+            ServerPlayer target;
+            try {
+                target = EntityArgument.getPlayer(ctx, "player");
+            } catch (CommandSyntaxException e) {
+                MessageUtil.sendActionBar(source, "tpa.player_not_found");
+                return Command.SINGLE_SUCCESS;
+            }
+
+            /* add request */
+            TpaRequest request = new TpaRequest(source, target, tpahere);
+
+            /* has similar request ? */
+            if (request.getSender().equals(request.getReceiver())) {
+                MessageUtil.sendActionBar(request.getSender(), "tpa.request_to_self");
+
+                return Command.SINGLE_SUCCESS;
+            }
+
+            if (requests.stream().anyMatch(request::similarTo)) {
+                MessageUtil.sendActionBar(request.getSender(), "tpa.similar_request_exists");
+                return Command.SINGLE_SUCCESS;
+            }
+
+            requests.add(request);
+            request.startTimeout();
+
+            /* feedback */
+            request.getReceiver().sendMessage(request.asReceiverComponent$Sent());
+            MentionPlayersJob.scheduleJob(request.getReceiver());
+            request.getSender().sendMessage(request.asSenderComponent$Sent());
             return Command.SINGLE_SUCCESS;
-        }
-
-        /* add request */
-        TpaRequest request = new TpaRequest(source, player, tpahere);
-
-        /* has similar request ? */
-        if (request.getSender().equals(request.getReceiver())) {
-            MessageUtil.sendActionBar(request.getSender(), "tpa.request_to_self");
-
-            return Command.SINGLE_SUCCESS;
-        }
-
-        if (requests.stream().anyMatch(request::similarTo)) {
-            MessageUtil.sendActionBar(request.getSender(), "tpa.similar_request_exists");
-            return Command.SINGLE_SUCCESS;
-        }
-
-        requests.add(request);
-        request.startTimeout();
-
-        /* feedback */
-        request.getReceiver().sendMessage(request.asReceiverComponent$Sent());
-        MentionPlayersJob.scheduleJob(request.getReceiver());
-        request.getSender().sendMessage(request.asSenderComponent$Sent());
-        return Command.SINGLE_SUCCESS;
+        });
     }
-
 
     private enum ResponseStatus {ACCEPT, DENY, CANCEL}
 }
