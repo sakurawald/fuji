3d45b9920d7b8390487af8f69841bbd661aaf4cc
sakurawald <sakurawald@gmail.com>
2024-06-17 21:59:14 +0800

    bump: minecraft 1.20.6

--- src/main/java/io/github/sakurawald/module/mixin/resource_world/EndGatewayBlockEntityMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/resource_world/EndGatewayBlockEntityMixin.java
@@ -2,6 +2,7 @@ package io.github.sakurawald.module.mixin.resource_world;
 
 import net.minecraft.block.entity.EndGatewayBlockEntity;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class EndGatewayBlockEntityMixin {
 
     // note: for a resource end, we force make World.END == World.END a true condition, so that the end gateway in resource end can work.
-    @Redirect(method = "tryTeleportingEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getRegistryKey()Lnet/minecraft/registry/RegistryKey;"))
-    private static RegistryKey<World> $tryTeleportingEntity(World instance) {
+    @Redirect(method = "getOrCreateExitPortalPos", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getRegistryKey()Lnet/minecraft/registry/RegistryKey;"))
+    private RegistryKey<World> $tryTeleportingEntity(ServerWorld instance) {
         return World.END;
     }
 }
