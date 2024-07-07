8fba23f5418d29cc97a15da2cd55d344ba47b499
sakurawald <sakurawald@gmail.com>
2024-07-08 06:37:45 +0800

    Revert "refactor: use custom events"
    This reverts commit 90db1a37

--- src/main/java/io/github/sakurawald/module/mixin/op_protect/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/op_protect/ServerPlayNetworkHandlerMixin.java
@@ -1,17 +1,18 @@
-package io.github.sakurawald.module.mixin._internal.event;
+package io.github.sakurawald.module.mixin.op_protect;
 
 
-import io.github.sakurawald.common.event.PrePlayerDisconnectEvent;
-import net.minecraft.entity.passive.SheepEntity;
+import io.github.sakurawald.Fuji;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.minecraft.network.DisconnectionInfo;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.ActionResult;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
@@ -19,13 +20,11 @@ public class ServerPlayNetworkHandlerMixin {
     @Shadow
     public ServerPlayerEntity player;
 
-    @Inject(at = @At(value = "HEAD"), method = "onDisconnected", cancellable = true)
+    @Inject(at = @At(value = "HEAD"), method = "onDisconnected")
     private void $onDisconnected(DisconnectionInfo disconnectionInfo, CallbackInfo ci) {
-        ActionResult result = PrePlayerDisconnectEvent.EVENT.invoker().interact(player, disconnectionInfo);
-        if (result == ActionResult.FAIL) {
-            ci.cancel();
+        if (Fuji.SERVER.getPlayerManager().isOperator(player.getGameProfile())) {
+            Fuji.LOGGER.info("op protect -> deop {}", player.getGameProfile().getName());
+            Fuji.SERVER.getPlayerManager().removeFromOperators(player.getGameProfile());
         }
     }
 }
-
-
