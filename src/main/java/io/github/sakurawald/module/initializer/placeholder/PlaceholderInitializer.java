c9fdf641d28549f4c571b6342d17504b330efd04
sakurawald <sakurawald@gmail.com>
2024-07-24 12:44:50 +0800

    revert: the mixin play + add: has permission, get meta, random, random player placeholder

--- src/main/java/io/github/sakurawald/module/initializer/placeholder/PlaceholderInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/placeholder/PlaceholderInitializer.java
@@ -4,7 +4,10 @@ import eu.pb4.placeholders.api.PlaceholderResult;
 import eu.pb4.placeholders.api.Placeholders;
 import io.github.sakurawald.Fuji;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
+import io.github.sakurawald.util.PermissionUtil;
+import io.github.sakurawald.util.RandomUtil;
 import io.github.sakurawald.util.ScheduleUtil;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
@@ -17,7 +20,9 @@ import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 
 @Slf4j
@@ -25,6 +30,54 @@ public class PlaceholderInitializer extends ModuleInitializer {
     private final Map<String, Map<String, String>> rotate = new HashMap<>();
 
     private static final String NO_PLAYER = "no player";
+
+    private void registerHasPermissionPlaceholder() {
+        Placeholders.register(Identifier.of(Fuji.MOD_ID, "has_permission"), (ctx, args) -> {
+            if (ctx.player() == null) {
+                return PlaceholderResult.invalid();
+            }
+
+            boolean value = PermissionUtil.hasPermission(ctx.player(), args);
+            return PlaceholderResult.value(Text.literal(String.valueOf(value)));
+        });
+    }
+
+    private void registerGetMetaPlaceholder() {
+        Placeholders.register(Identifier.of(Fuji.MOD_ID, "get_meta"), (ctx, args) -> {
+            if (ctx.player() == null) {
+                return PlaceholderResult.invalid();
+            }
+
+            Optional<String> o = PermissionUtil.getMeta(ctx.player(), args, String::valueOf);
+            String value = o.orElse("NO_EXIST");
+            return PlaceholderResult.value(Text.literal(value));
+        });
+    }
+
+    private void registerRandomPlayerPlaceholder() {
+        Placeholders.register(Identifier.of(Fuji.MOD_ID, "random_player"), (ctx, args) -> {
+            List<ServerPlayerEntity> playerList = ctx.server().getPlayerManager().getPlayerList();
+            ServerPlayerEntity serverPlayerEntity = RandomUtil.drawList(playerList);
+            return PlaceholderResult.value(Text.literal(serverPlayerEntity.getGameProfile().getName()));
+        });
+    }
+
+    private void registerRandomPlaceholder() {
+        Placeholders.register(Identifier.of(Fuji.MOD_ID, "random"), (ctx, args) -> {
+            if (args == null) return PlaceholderResult.invalid();
+            String[] split = args.split(" ");
+            if (split.length != 2) return PlaceholderResult.invalid();
+
+            int i;
+            try {
+                i = RandomUtil.getRng().nextInt(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+            } catch (Exception e) {
+                return PlaceholderResult.invalid();
+            }
+
+            return PlaceholderResult.value(Text.literal(String.valueOf(i)));
+        });
+    }
 
     private void registerHealthBarPlaceholder() {
         Placeholders.register(Identifier.of(Fuji.MOD_ID, "health_bar"), (ctx, args) -> {
@@ -123,6 +176,10 @@ public class PlaceholderInitializer extends ModuleInitializer {
                 });
         registerHealthBarPlaceholder();
         registerRotatePlaceholder();
+        registerHasPermissionPlaceholder();
+        registerGetMetaPlaceholder();
+        registerRandomPlayerPlaceholder();
+        registerRandomPlaceholder();
 
         /* events */
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
