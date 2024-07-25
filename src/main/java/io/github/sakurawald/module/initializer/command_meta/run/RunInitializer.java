6d9607ce4698ba9aa85306c975148d53a770ef22
sakurawald <sakurawald@gmail.com>
2024-07-26 03:59:50 +0800

    fix: .gitignore

---
+++ src/main/java/io/github/sakurawald/module/initializer/command_meta/run/RunInitializer.java
@@ -0,0 +1,41 @@
+package io.github.sakurawald.module.initializer.command_meta.run;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import io.github.sakurawald.module.common.structure.CommandExecuter;
+import io.github.sakurawald.module.initializer.ModuleInitializer;
+import io.github.sakurawald.util.minecraft.CommandHelper;
+import lombok.SneakyThrows;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class RunInitializer extends ModuleInitializer {
+    @Override
+    public void registerCommand(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
+        dispatcher.register(literal("run")
+                .then(literal("as")
+                        .then(literal("player").then(CommandHelper.Argument.player().then(CommandHelper.Argument.rest().executes(this::runAsPlayer))))
+                        .then(literal("console").then(CommandHelper.Argument.rest().executes(this::runAsConsole)))));
+
+    }
+
+    private int runAsConsole(CommandContext<ServerCommandSource> ctx) {
+        String rest = CommandHelper.Argument.rest(ctx);
+
+        CommandExecuter.executeCommandAsConsole(null, rest);
+        return CommandHelper.Return.SUCCESS;
+    }
+
+    @SneakyThrows
+    private int runAsPlayer(CommandContext<ServerCommandSource> ctx) {
+        ServerPlayerEntity player = CommandHelper.Argument.player(ctx);
+        String rest = CommandHelper.Argument.rest(ctx);
+
+        CommandExecuter.executeCommandAsPlayer(player, rest);
+        return CommandHelper.Return.SUCCESS;
+    }
+}
