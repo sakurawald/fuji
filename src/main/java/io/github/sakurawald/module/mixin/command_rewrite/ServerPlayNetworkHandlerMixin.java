bbc7e92508956fbcf8a72c82e95f8d1ef3bc9120
sakurawald <sakurawald@gmail.com>
2024-07-30 23:43:11 +0800

    feature: friendly data validate for `command_rewrite` module

--- src/main/java/io/github/sakurawald/module/mixin/command_rewrite/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/command_rewrite/ServerPlayNetworkHandlerMixin.java
@@ -2,6 +2,7 @@ package io.github.sakurawald.module.mixin.command_rewrite;
 
 import io.github.sakurawald.config.Configs;
 import io.github.sakurawald.module.common.structure.RegexRewriteEntry;
+import io.github.sakurawald.util.LogUtil;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +15,11 @@ public class ServerPlayNetworkHandlerMixin {
     @ModifyVariable(method = "executeCommand", at = @At(value = "HEAD"), ordinal = 0, argsOnly = true)
     public String interceptPacketsOfIssuedCommand(@NotNull String string) {
         for (RegexRewriteEntry entry : Configs.configHandler.model().modules.command_rewrite.regex) {
+            if (entry.regex == null || entry.replacement == null) {
+                LogUtil.warn("There is an invalid `null` entry in `command_rewrite.regex`, you should remove it.");
+                continue;
+            }
+
             if (string.matches(entry.regex)) {
                 return string.replaceAll(entry.regex, entry.replacement);
             }
