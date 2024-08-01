7ec0ab099e08b9d4fc91fe5990145d99355833e1
sakurawald <sakurawald@gmail.com>
2024-08-01 13:13:20 +0800

    fix: required dependency mod checker

--- src/main/java/io/github/sakurawald/module/ModuleManager.java
+++ src/main/java/io/github/sakurawald/module/ModuleManager.java
@@ -134,7 +134,7 @@ public class ModuleManager extends AbstractManager {
     private  boolean isRequiredModsInstalled(@NotNull List<String> packagePath) {
         String basePackagePath = packagePath.getFirst();
 
-        if (basePackagePath.equals("carpet")) {
+        if (packagePath.equals(List.of("gameplay", "carpet"))) {
             return FabricLoader.getInstance().isModLoaded("carpet");
         }
 
