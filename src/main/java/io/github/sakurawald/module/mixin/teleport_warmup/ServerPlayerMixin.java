f01816878ef1d93bcc4879ef852d58e27087f332
sakurawald <sakurawald@gmail.com>
2024-08-26 09:14:58 +0800

    refactor: change the package of @Nullable

--- src/main/java/io/github/sakurawald/module/mixin/teleport_warmup/ServerPlayerMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/teleport_warmup/ServerPlayerMixin.java
@@ -12,14 +12,13 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import javax.annotation.Nullable;
 
 @Mixin(value = ServerPlayerEntity.class, priority = 1000 - 500)
 public abstract class ServerPlayerMixin {
