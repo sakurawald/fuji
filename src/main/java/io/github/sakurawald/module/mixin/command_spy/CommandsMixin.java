3be661a759254db2b20c89c5ce28940853388990
sakurawald <sakurawald@gmail.com>
2024-09-01 11:28:02 +0800

    refactor: add the command prefix for command_syp module

--- src/main/java/io/github/sakurawald/module/mixin/command_spy/CommandsMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/command_spy/CommandsMixin.java
@@ -22,6 +22,6 @@ public class CommandsMixin {
         if (player == null) return;
 
         // fix: fabric console will not log the command issue
-        LogUtil.info("{} issued server command: {}", player.getGameProfile().getName(), string);
+        LogUtil.info("{} issued server command: /{}", player.getGameProfile().getName(), string);
     }
 }
