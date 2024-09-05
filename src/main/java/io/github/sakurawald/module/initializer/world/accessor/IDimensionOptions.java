20e303345b12c592f4fc76acf6bd9a412d2fa6a8
sakurawald <sakurawald@gmail.com>
2024-09-05 15:44:57 +0800

    build: change the intro in fabric.mod.json

--- src/main/java/io/github/sakurawald/module/initializer/world/accessor/IDimensionOptions.java
+++ src/main/java/io/github/sakurawald/module/initializer/world/accessor/IDimensionOptions.java
@@ -5,7 +5,6 @@ import org.jetbrains.annotations.ApiStatus;
 
 import java.util.function.Predicate;
 
-@ApiStatus.Internal
 public interface IDimensionOptions {
     Predicate<DimensionOptions> SAVE_PROPERTIES_PREDICATE = (e) -> ((IDimensionOptions) (Object) e).fuji$getSaveProperties();
 
