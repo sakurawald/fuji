f85ea6a7c390aa327b485edaf271289bf08def5f
sakurawald <sakurawald@gmail.com>
2024-09-05 10:41:13 +0800

    refactor: use abst instead of impl for collection

--- src/main/java/io/github/sakurawald/module/initializer/home/HomeInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/home/HomeInitializer.java
@@ -1,17 +1,17 @@
 package io.github.sakurawald.module.initializer.home;
 
+import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
+import io.github.sakurawald.core.auxiliary.minecraft.MessageHelper;
+import io.github.sakurawald.core.auxiliary.minecraft.PermissionHelper;
 import io.github.sakurawald.core.command.annotation.CommandNode;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.core.config.handler.abst.ConfigHandler;
 import io.github.sakurawald.core.config.handler.impl.ObjectConfigHandler;
-import io.github.sakurawald.module.initializer.home.config.model.HomeModel;
 import io.github.sakurawald.core.manager.impl.scheduler.ScheduleManager;
 import io.github.sakurawald.core.structure.Position;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import io.github.sakurawald.module.initializer.home.command.argument.wrapper.HomeName;
-import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
-import io.github.sakurawald.core.auxiliary.minecraft.MessageHelper;
-import io.github.sakurawald.core.auxiliary.minecraft.PermissionHelper;
+import io.github.sakurawald.module.initializer.home.config.model.HomeModel;
 import lombok.Getter;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.jetbrains.annotations.NotNull;
--- src/main/java/io/github/sakurawald/module/initializer/works/structure/WorksCache.java
+++ src/main/java/io/github/sakurawald/module/initializer/works/structure/WorksCache.java
@@ -6,15 +6,16 @@ import net.minecraft.util.math.BlockPos;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 
 public class WorksCache {
 
     @Getter
-    private static final ConcurrentHashMap<BlockPos, HashSet<Work>> blockpos2works = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<BlockPos, Set<Work>> blockpos2works = new ConcurrentHashMap<>();
     @Getter
-    private static final ConcurrentHashMap<Integer, HashSet<Work>> entity2works = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Integer, Set<Work>> entity2works = new ConcurrentHashMap<>();
 
     public static void bind(BlockPos blockPos, Work work) {
         blockpos2works.computeIfAbsent(blockPos, k -> new HashSet<>()).add(work);
@@ -25,17 +26,17 @@ public class WorksCache {
     }
 
     public static void unbind(Work work) {
-        Iterator<Map.Entry<BlockPos, HashSet<Work>>> iter1 = blockpos2works.entrySet().iterator();
+        Iterator<Map.Entry<BlockPos, Set<Work>>> iter1 = blockpos2works.entrySet().iterator();
         while (iter1.hasNext()) {
-            Map.Entry<BlockPos, HashSet<Work>> entry = iter1.next();
+            Map.Entry<BlockPos, Set<Work>> entry = iter1.next();
             entry.getValue().remove(work);
             if (entry.getValue().isEmpty()) {
                 iter1.remove();
             }
         }
-        Iterator<Map.Entry<Integer, HashSet<Work>>> iter2 = entity2works.entrySet().iterator();
+        Iterator<Map.Entry<Integer, Set<Work>>> iter2 = entity2works.entrySet().iterator();
         while (iter2.hasNext()) {
-            Map.Entry<Integer, HashSet<Work>> entry = iter2.next();
+            Map.Entry<Integer, Set<Work>> entry = iter2.next();
             entry.getValue().remove(work);
             if (entry.getValue().isEmpty()) {
                 iter2.remove();
