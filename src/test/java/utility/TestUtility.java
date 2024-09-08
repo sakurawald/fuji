3dbab2af684bf151acaa37d443f7193c75670a1f
sakurawald <sakurawald@gmail.com>
2024-09-08 11:32:06 +0800

    refactor: utility compile-time reflection, now the graph scanner library is excluded. (reduce the size of released jar file)

---
+++ src/test/java/utility/TestUtility.java
@@ -0,0 +1,15 @@
+package utility;
+
+import io.github.classgraph.ClassGraph;
+import io.github.sakurawald.Fuji;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestUtility {
+
+    public static ClassGraph makeBaseClassGraph() {
+        return new ClassGraph()
+            .acceptPackages(Fuji.class.getPackageName());
+    }
+
+}
