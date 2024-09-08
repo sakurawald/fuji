937bb1b3b61ba8d8f170faad59ea4037d9f22279
sakurawald <sakurawald@gmail.com>
2024-09-08 13:43:37 +0800

    refactor: use static inner class to represent the configuration structure.

--- src/main/java/io/github/sakurawald/module/initializer/chat/config/model/ChatModel.java
+++ src/main/java/io/github/sakurawald/module/initializer/chat/config/model/ChatModel.java
@@ -4,12 +4,11 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 
-@SuppressWarnings("InnerClassMayBeStatic")
 public class ChatModel {
 
     public @NotNull Format format = new Format();
 
-    public class Format {
+    public static class Format {
         public @NotNull HashMap<String, String> player2format = new HashMap<>() {
             {
                 this.put("Steve", "<#FFC7EA>%message%");
