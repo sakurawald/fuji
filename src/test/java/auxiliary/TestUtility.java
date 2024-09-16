7febd0b20c58e93dd8401c7916eaea9fd94db27f
sakurawald <sakurawald@gmail.com>
2024-09-16 12:59:41 +0800

    add: dependency checkers to scan source file.

--- src/test/java/auxiliary/TestUtility.java
+++ src/test/java/auxiliary/TestUtility.java
@@ -4,6 +4,11 @@ import io.github.classgraph.ClassGraph;
 import io.github.sakurawald.Fuji;
 import lombok.experimental.UtilityClass;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @UtilityClass
 public class TestUtility {
 
@@ -12,4 +17,15 @@ public class TestUtility {
             .acceptPackages(Fuji.class.getPackageName());
     }
 
+    @SuppressWarnings("SameParameterValue")
+    public static List<String> extractMatches(Pattern pattern, String text, int group) {
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> ret = new ArrayList<>();
+        while (matcher.find()) {
+            ret.add(matcher.group(group));
+        }
+
+        return ret;
+    }
 }
