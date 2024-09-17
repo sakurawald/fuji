5da7afcf10dc34c8c16f29e3fc8d91906c161cd9
sakurawald <sakurawald@gmail.com>
2024-09-17 16:24:55 +0800

    refactor: rename ConfigHandler to ConfigurationHandler

--- src/main/java/io/github/sakurawald/core/config/Configs.java
+++ src/main/java/io/github/sakurawald/core/config/Configs.java
@@ -1,12 +1,12 @@
 package io.github.sakurawald.core.config;
 
-import io.github.sakurawald.core.config.handler.abst.ConfigHandler;
-import io.github.sakurawald.core.config.handler.impl.ObjectConfigHandler;
+import io.github.sakurawald.core.config.handler.abst.ConfigurationHandler;
+import io.github.sakurawald.core.config.handler.impl.ObjectConfigurationHandler;
 import io.github.sakurawald.core.config.model.ConfigModel;
 
 
 public class Configs {
 
-    public static final ConfigHandler<ConfigModel> configHandler = new ObjectConfigHandler<>("config.json", ConfigModel.class);
+    public static final ConfigurationHandler<ConfigModel> configHandler = new ObjectConfigurationHandler<>("config.json", ConfigModel.class);
 
 }
