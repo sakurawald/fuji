786259b4fd2e568d9dfb0687b5e04e510f72f46f
sakurawald <sakurawald@gmail.com>
2024-09-18 11:38:01 +0800

    refactor: use lazy-loading for all managers. (avoid triggering the initialization codes too early, and enhance the compatibility with other mods)

--- src/main/java/io/github/sakurawald/core/manager/Managers.java
+++ src/main/java/io/github/sakurawald/core/manager/Managers.java
@@ -10,26 +10,29 @@ import io.github.sakurawald.core.manager.impl.module.ModuleManager;
 import io.github.sakurawald.core.manager.impl.scheduler.ScheduleManager;
 import lombok.Getter;
 
+/**
+ * To avoid trigger the initialization of static field too early, we should use `lazy` loading.
+ */
 public class Managers {
 
-    @Getter
+    @Getter(lazy = true)
     private static final ModuleManager moduleManager = new ModuleManager();
 
-    @Getter
+    @Getter(lazy = true)
     private static final BossBarManager bossBarManager = new BossBarManager();
 
-    @Getter
+    @Getter(lazy = true)
     private static final ScheduleManager scheduleManager = new ScheduleManager();
 
-    @Getter
+    @Getter(lazy = true)
     private static final BaseBackupManager standardBackupManager = new StandardBackupManager();
 
-    @Getter
+    @Getter(lazy = true)
     private static final BaseBackupManager rescueBackupManager = new BackupRescueManager();
 
-    @Getter
+    @Getter(lazy = true)
     private static final AttachmentManager attachmentManager = new AttachmentManager();
 
-    @Getter
+    @Getter(lazy = true)
     private static final CommandManager commandManager = new CommandManager();
 }
