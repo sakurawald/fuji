ce7ef1c5450e97f5146414cc8770221679e3c1bd
sakurawald <sakurawald@gmail.com>
2024-09-18 18:10:16 +0800

    feature: add module flatten transformer

--- src/main/java/io/github/sakurawald/core/config/Configs.java
+++ src/main/java/io/github/sakurawald/core/config/Configs.java
@@ -3,7 +3,7 @@ package io.github.sakurawald.core.config;
 import io.github.sakurawald.core.config.handler.abst.BaseConfigurationHandler;
 import io.github.sakurawald.core.config.handler.impl.ObjectConfigurationHandler;
 import io.github.sakurawald.core.config.model.ConfigModel;
-
+import io.github.sakurawald.core.config.transformer.impl.FlattenModulesTransformer;
 
 public class Configs {
 
@@ -12,6 +12,7 @@ public class Configs {
             // enable detection for main-control file.
             this.detectUnknownKeysFlag = true;
         }
-    };
+    }
+        .addTransformer(new FlattenModulesTransformer());
 
 }
