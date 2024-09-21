eb9bfce97ef6529a97effd89f5c321671aa06695
sakurawald <sakurawald@gmail.com>
2024-09-21 11:37:55 +0800

    refactor: makeDocumentContext() manually, to avoid loading a not exists file in file system.

--- src/main/java/io/github/sakurawald/core/config/transformer/impl/MoveFileTransformer.java
+++ src/main/java/io/github/sakurawald/core/config/transformer/impl/MoveFileTransformer.java
@@ -1,5 +1,6 @@
 package io.github.sakurawald.core.config.transformer.impl;
 
+import io.github.sakurawald.core.auxiliary.LogUtil;
 import io.github.sakurawald.core.config.transformer.abst.ConfigurationTransformer;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -10,16 +11,19 @@ import java.nio.file.Path;
 @AllArgsConstructor
 public class MoveFileTransformer extends ConfigurationTransformer {
 
-    final Path destination;
+    Path source;
+    Path destination;
 
     @SneakyThrows
     @Override
     public void apply() {
-        if (Files.notExists(this.getPath()) || Files.exists(destination)) return;
+        destination = destination.resolve(this.getPath().toFile().getName());
 
-        Files.createDirectories(this.destination);
+        if (Files.notExists(this.source) || Files.exists(destination)) return;
+
+        Files.createDirectories(this.destination.getParent());
         logConsole("move to {}", destination);
-        Files.move(this.getPath(), destination.resolve(this.getPath().toFile().getName()));
+        Files.move(source, destination);
     }
 
 }
