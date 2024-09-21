717e6be07cd3d0c93555d6fc5b59cb2593b970d1
sakurawald <sakurawald@gmail.com>
2024-09-21 13:37:59 +0800

    refactor: use @SerializedName annotation to handle field rename operation

--- src/main/java/io/github/sakurawald/module/initializer/command_toolbox/warp/config/model/WarpModel.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_toolbox/warp/config/model/WarpModel.java
@@ -1,5 +1,6 @@
 package io.github.sakurawald.module.initializer.command_toolbox.warp.config.model;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.sakurawald.module.initializer.command_toolbox.warp.structure.WarpEntry;
 import org.jetbrains.annotations.NotNull;
 
@@ -7,5 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WarpModel {
+    @SerializedName(value = "name2warp", alternate = "warps")
     public @NotNull Map<String, WarpEntry> name2warp = new HashMap<>();
 }
--- src/main/java/io/github/sakurawald/module/initializer/home/config/model/HomeModel.java
+++ src/main/java/io/github/sakurawald/module/initializer/home/config/model/HomeModel.java
@@ -1,5 +1,6 @@
 package io.github.sakurawald.module.initializer.home.config.model;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.sakurawald.core.structure.SpatialPose;
 import org.jetbrains.annotations.NotNull;
 
@@ -8,5 +9,6 @@ import java.util.Map;
 
 public class HomeModel {
 
+    @SerializedName(value = "name2home", alternate = "homes")
     public @NotNull Map<String, Map<String, SpatialPose>> name2home = new HashMap<>();
 }
