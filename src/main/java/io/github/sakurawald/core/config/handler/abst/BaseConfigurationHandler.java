e7a9b2f5091fae0176b3fed5dc40d2c2095f8e8f
sakurawald <sakurawald@gmail.com>
2024-09-21 13:47:06 +0800

    refactor: simplify the merge trees operation. (now the operation will be done by gson.)

--- src/main/java/io/github/sakurawald/core/config/handler/abst/BaseConfigurationHandler.java
+++ src/main/java/io/github/sakurawald/core/config/handler/abst/BaseConfigurationHandler.java
@@ -49,6 +49,8 @@ import java.util.regex.Pattern;
  * 1. Only use static inner class in config model java object, this is because a historical design problem in java.
  * 2. The new gson type adapter should be registered before the call to loadFromDisk()
  * 3. The type system of java is static, given an object instance, you can use instance.getClass() to get the type of the instance, which means that you don't need to specify the typeOfT for gson library.
+ *
+ *
  * <p>
  * The configuration handler in module initializer should be static:
  * 1. The major point to make configuration handler a member of class, is that it's easier to control the lifecycle of objects, however, considering the fact that the configuration handler is a mapping between file system and memory, it should be static and unique.
@@ -74,15 +76,8 @@ public abstract class BaseConfigurationHandler<T> {
 
     protected T model;
 
-
-    /* flags */
-    private boolean alreadyBackupFlag = false;
-    private boolean writeStorageWithDataTreeFlag = false;
-    private boolean exitJvmFlag = false;
-
     /* transformer */
     private final List<ConfigurationTransformer> transformers = new ArrayList<>();
-
 
     public BaseConfigurationHandler<T> addTransformer(ConfigurationTransformer transformer) {
         this.transformers.add(transformer);
@@ -179,30 +174,14 @@ public abstract class BaseConfigurationHandler<T> {
                 // treat all keys as lower-underscore format
                 renameKeysToLowerUnderscoreStyle(dataTree.getAsJsonObject());
 
-                // merge data tree with schema tree
+                // merge data tree with schema tree: the gson.fromJson() will use defaultModel as the schema tree to generate missing default kv-pairs for data tree.
                 T defaultModel = getDefaultModel();
-                JsonElement schemaTree = gson.toJsonTree(defaultModel);
-                mergeJsonTree("", dataTree.getAsJsonObject(), schemaTree.getAsJsonObject());
-
-                // handle flags
-                if (this.writeStorageWithDataTreeFlag) {
-                    this.writeStorageWithDataTreeFlag = false;
-                    Files.writeString(this.path, gson.toJson(dataTree));
-                }
-
-                if (this.exitJvmFlag) {
-                    LogUtil.info("sorry, i have to shutdown the server because there is an error existing in the configuration file `{}`", this.path);
-                    System.exit(-1);
-                }
-
-                // use merged tree
                 this.model = (T) gson.fromJson(dataTree, defaultModel.getClass());
 
                 /* write storage back, to:
                  * 1. keep the sync between memory and disk.
                  * 2. trigger the field naming conversion in gson.
                  * */
-
                 this.writeStorage();
             }
 
@@ -252,102 +231,6 @@ public abstract class BaseConfigurationHandler<T> {
             LogUtil.debug("write storage on server stopping: {}", this.path);
             this.writeStorage();
         });
-    }
-
-    protected void mergeJsonTree(String parentPath, @NotNull JsonObject dataTree, @NotNull JsonObject schemaTree) {
-        /* navigating using schema tree */
-        Set<Map.Entry<String, JsonElement>> entrySet = schemaTree.entrySet();
-        for (Map.Entry<String, JsonElement> entry : entrySet) {
-            String key = entry.getKey();
-            JsonElement value = entry.getValue();
-
-            /* is the key missing form the data tree ? */
-            if (dataTree.has(key)) {
-                String currentPath = StringUtils.strip(parentPath + "." + key, ".");
-
-                // is the type of value equals ?
-                if (JsonUtil.sameType(dataTree.get(key), value)) {
-
-                    // are they not atoms ?
-                    if (dataTree.get(key).isJsonObject() && value.isJsonObject()) {
-
-                        /*
-                        Gson will store the Map type as JsonObject, we can't tell if the type is a Map or a Java Object by the value.
-                        We have to tell them by the name of the key: if its name matches the MAP_TYPE_MATCHER, then we indicate it is a Map, and skip it.
-                         */
-                        if (MAP_TYPE_MATCHER.matcher(key).matches()) {
-                            continue;
-                        }
-
-                        // flatten the tree
-                        mergeJsonTree(currentPath, dataTree.getAsJsonObject(key), value.getAsJsonObject());
-                    }
-
-                } else {
-                    handleTreeMismatch(dataTree, currentPath, key, value);
-                }
-
-            } else {
-                /* for JsonArray type, we will not walk into it, which means that the size of JsonArray will not be changed.
-                However, each time the gson serialize the java object with its type specifier, gson will add the missing keys in the json using the field initialization-form defined in java.
-
-                Consider the fact that, we only store user-generated-data using array and map.
-                 */
-                LogUtil.warn("add missing json key-value pair: file = {}, key = {}, value = {}", this.path.toFile().getName(), key, value);
-                dataTree.add(key, value);
-                this.writeStorageWithDataTreeFlag = true;
-            }
-        }
-    }
-
-    @SneakyThrows
-    private void handleTreeMismatch(@NotNull JsonObject dataTree, String currentPath, String key, JsonElement value) {
-        while (true) {
-            LogUtil.error("""
-
-                # What happened ?
-                There is an incompatibility issue in the configuration file `{}`.
-                  - Actual type of key `{}` does not match the expected type.
-
-                # Possible reasons:
-                  1. In the new version of fuji, the value of the key has changed its type.
-                  2. The configuration file was been modified incorrectly.
-
-                # What can I do ?
-                  - Press "y" and enter: backup the `config/fuji/` directory and `override the key with default value`.
-                  - Press "n" and enter: ignore this issue.
-                  - Press "q" and enter: abort the server start-up process.
-
-                """, this.path.toFile().getAbsoluteFile(), currentPath, this.path.toFile().getAbsoluteFile(), currentPath);
-
-            /* ynop query */
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.next().trim();
-            if (input.equalsIgnoreCase("y")) {
-                if (!this.alreadyBackupFlag) {
-                    Managers.getRescueBackupManager().backup();
-                    LogUtil.info("backup the `config/fuji` directory into `config/fuji/backup_rescue` directory successfully.");
-                    this.alreadyBackupFlag = true;
-                }
-
-                // override it with default value
-                LogUtil.warn("override the key `{}` with default value: {}", key, value);
-                dataTree.remove(key);
-                dataTree.add(key, value);
-
-                // data tree is modified in the memory, also we should sync the modification into the storage.
-                writeStorageWithDataTreeFlag = true;
-                break;
-            } else if (input.equalsIgnoreCase("n")) {
-                this.exitJvmFlag = true;
-                break;
-            } else if (input.equalsIgnoreCase("q")) {
-                System.exit(-1);
-                break;
-            }
-
-            LogUtil.error("Invalid input.");
-        }
     }
 
     public T getModel() {
