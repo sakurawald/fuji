839ee489f6bfce2d1ef5d150faeefcfb38c2210e
sakurawald <sakurawald@gmail.com>
2024-09-21 13:44:35 +0800

    refactor: move `functional.enchantment.override_power` module into `functional.enchantment` module. (now it's always enabled with `functional.enchantment` module)

--- src/main/java/io/github/sakurawald/core/config/model/ConfigModel.java
+++ src/main/java/io/github/sakurawald/core/config/model/ConfigModel.java
@@ -353,13 +353,6 @@ public class ConfigModel {
 
             public static class Enchantment {
                 public boolean enable = false;
-
-                public OverridePower override_power = new OverridePower();
-
-                public static class OverridePower {
-                    public boolean enable = true;
-                }
-
             }
 
             public static class GrindStone {
