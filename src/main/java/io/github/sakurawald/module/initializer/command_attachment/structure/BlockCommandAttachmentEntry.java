3d403b01e16498536dec55304a9d1710ffa194fc
sakurawald <sakurawald@gmail.com>
2024-09-21 21:40:12 +0800

    feature: add created_in field for block attachment

--- src/main/java/io/github/sakurawald/module/initializer/command_attachment/structure/BlockCommandAttachmentEntry.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_attachment/structure/BlockCommandAttachmentEntry.java
@@ -4,7 +4,11 @@ import io.github.sakurawald.module.initializer.command_attachment.command.argume
 import io.github.sakurawald.module.initializer.command_attachment.command.argument.wrapper.InteractType;
 
 public class BlockCommandAttachmentEntry extends CommandAttachmentEntry {
-    public BlockCommandAttachmentEntry(String command, InteractType interactType, ExecuteAsType executeAsType, int maxUseTimes, int useTimes) {
+
+    String created_in;
+
+    public BlockCommandAttachmentEntry(String created_in, String command, InteractType interactType, ExecuteAsType executeAsType, int maxUseTimes, int useTimes) {
         super(CommandAttackmentType.BLOCK, command, interactType, executeAsType, maxUseTimes, useTimes);
+        this.created_in = created_in;
     }
 }
