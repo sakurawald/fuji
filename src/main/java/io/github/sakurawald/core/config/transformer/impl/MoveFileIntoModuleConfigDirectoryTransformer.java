eee03ab050156afc3695f9cf6230cac163f73f3d
sakurawald <sakurawald@gmail.com>
2024-09-21 11:39:33 +0800

    fix: move file into module transformer should not depend on the associated file.

--- src/main/java/io/github/sakurawald/core/config/transformer/impl/MoveFileIntoModuleConfigDirectoryTransformer.java
+++ src/main/java/io/github/sakurawald/core/config/transformer/impl/MoveFileIntoModuleConfigDirectoryTransformer.java
@@ -2,10 +2,12 @@ package io.github.sakurawald.core.config.transformer.impl;
 
 import io.github.sakurawald.core.auxiliary.ReflectionUtil;
 
+import java.nio.file.Path;
 
-public class MoveFileIntoModuleConfigDirectoryTransformer extends MoveFileTransformer{
 
-    public MoveFileIntoModuleConfigDirectoryTransformer(Class<?> clazz) {
-        super(ReflectionUtil.getModuleConfigPath(clazz));
+public class MoveFileIntoModuleConfigDirectoryTransformer extends MoveFileTransformer {
+
+    public MoveFileIntoModuleConfigDirectoryTransformer(Path source, Class<?> clazz) {
+        super(source, ReflectionUtil.getModuleConfigPath(clazz));
     }
 }
