05f8ea3e293ddf952f12e2030a587513b2052ee1
sakurawald <sakurawald@gmail.com>
2024-09-22 19:04:58 +0800

    refactor: use vanilla title api

--- src/main/java/io/github/sakurawald/module/initializer/echo/send_title/SendTitleInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/echo/send_title/SendTitleInitializer.java
@@ -7,11 +7,12 @@ import io.github.sakurawald.core.command.annotation.CommandNode;
 import io.github.sakurawald.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.title.Title;
-import net.kyori.adventure.util.Ticks;
+import net.minecraft.network.packet.s2c.play.SubtitleS2CPacket;
+import net.minecraft.network.packet.s2c.play.TitleFadeS2CPacket;
+import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 import java.util.Optional;
 
@@ -33,11 +34,12 @@ public class SendTitleInitializer extends ModuleInitializer {
         int $stayTicks = stayTicks.orElse(70);
         int $fadeOutTicks = fadeOutTicks.orElse(20);
 
-        Component mainTitleComponent = LocaleHelper.getTextByValue(player, $mainTitle).asComponent();
-        Component subTitleComponent = LocaleHelper.getTextByValue(player, $subTitle).asComponent();
-        Title.Times times = Title.Times.times(Ticks.duration($fadeInTicks), Ticks.duration($stayTicks), Ticks.duration($fadeOutTicks));
-        Title title = Title.title(mainTitleComponent, subTitleComponent, times);
-        player.showTitle(title);
+        Text mainTitleComponent = LocaleHelper.getTextByValue(player, $mainTitle);
+        Text subTitleComponent = LocaleHelper.getTextByValue(player, $subTitle);
+
+        player.networkHandler.sendPacket(new TitleFadeS2CPacket($fadeInTicks, $stayTicks, $fadeOutTicks));
+        player.networkHandler.sendPacket(new TitleS2CPacket(mainTitleComponent));
+        player.networkHandler.sendPacket(new SubtitleS2CPacket(subTitleComponent));
 
         LocaleHelper.sendMessageByKey(ctx.getSource(), "operation.success");
         return CommandHelper.Return.SUCCESS;
