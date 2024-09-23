6a2f8670c3001d2b30cf986413f4f6be3d64746a
sakurawald <sakurawald@gmail.com>
2024-09-23 16:08:46 +0800

    refactor: use vanilla text api

--- src/main/java/io/github/sakurawald/module/initializer/top_chunks/TopChunksInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/top_chunks/TopChunksInitializer.java
@@ -10,8 +10,6 @@ import io.github.sakurawald.core.config.handler.impl.ObjectConfigurationHandler;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import io.github.sakurawald.module.initializer.top_chunks.config.model.TopChunksConfigModel;
 import io.github.sakurawald.module.initializer.top_chunks.structure.ChunkScore;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -19,6 +17,8 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -75,17 +75,17 @@ public class TopChunksInitializer extends ModuleInitializer {
             var config = TopChunksInitializer.config.getModel();
             calculateNearestPlayer(ctx.getSource(), PQ, config.top.rows * config.top.columns);
 
-            TextComponent.Builder textComponentBuilder = Component.text();
+            MutableText textComponentBuilder = Text.empty();
             outer:
             for (int j = 0; j < config.top.rows; j++) {
                 for (int i = 0; i < config.top.columns; i++) {
                     if (PQ.isEmpty()) break outer;
-                    textComponentBuilder.append(PQ.poll().asText(ctx.getSource())).appendSpace();
+                    textComponentBuilder.append(PQ.poll().asText(ctx.getSource())).append(LocaleHelper.TEXT_SPACE);
                 }
-                textComponentBuilder.append(Component.newline());
+                textComponentBuilder.append(LocaleHelper.TEXT_NEWLINE);
             }
 
-            ctx.getSource().sendMessage(textComponentBuilder.asComponent());
+            ctx.getSource().sendMessage(textComponentBuilder);
         });
 
         return CommandHelper.Return.SUCCESS;
