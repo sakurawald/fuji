a6bf2f40bddaecb7da27e4b86cc285538b69e95d
sakurawald <sakurawald@gmail.com>
2024-09-24 08:17:08 +0800

    refactor: simplify the AbortException object

--- src/main/java/io/github/sakurawald/core/command/exception/AbortOperationException.java
+++ src/main/java/io/github/sakurawald/core/command/exception/AbortOperationException.java
@@ -2,10 +2,6 @@ package io.github.sakurawald.core.command.exception;
 
 public class AbortOperationException extends RuntimeException {
 
-    public AbortOperationException(String message) {
-        super(message);
-    }
-
     public AbortOperationException() {
     }
 }
