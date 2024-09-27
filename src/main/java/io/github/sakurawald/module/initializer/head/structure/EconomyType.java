83f4dd90460b794a635d2c119fa71e4470920c56
sakurawald <sakurawald@gmail.com>
2024-09-27 14:49:24 +0800

    refactor: the extract() method

--- src/main/java/io/github/sakurawald/module/initializer/head/structure/EconomyType.java
+++ src/main/java/io/github/sakurawald/module/initializer/head/structure/EconomyType.java
@@ -2,29 +2,46 @@ package io.github.sakurawald.module.initializer.head.structure;
 
 import io.github.sakurawald.core.auxiliary.minecraft.RegistryHelper;
 import io.github.sakurawald.module.initializer.head.HeadInitializer;
-import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.item.PlayerInventoryStorage;
-import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
 
 public enum EconomyType {
     ITEM,
     FREE;
+
+    @SuppressWarnings("WhileLoopReplaceableByForEach")
+    private static boolean extract(ServerPlayerEntity player, @NotNull Item item, int amount) {
+        Iterator<DefaultedList<ItemStack>> iterator = player.getInventory().combinedInventory.iterator();
+        while (iterator.hasNext()) {
+            DefaultedList<ItemStack> list = iterator.next();
+
+            for (ItemStack itemStack : list) {
+                if (itemStack.getItem().equals(item)
+                    && !itemStack.hasEnchantments()
+                    && itemStack.getCount() >= amount
+                ) {
+                    itemStack.decrement(amount);
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
 
     public static void tryPurchase(@NotNull ServerPlayerEntity player, int amount, @NotNull Runnable onPurchase) {
         int trueAmount = amount * HeadInitializer.headHandler.getModel().cost_amount;
         switch (HeadInitializer.headHandler.getModel().economy_type) {
             case FREE -> onPurchase.run();
             case ITEM -> {
-                try (Transaction transaction = Transaction.openOuter()) {
-                    long extracted = PlayerInventoryStorage.of(player).extract(ItemVariant.of(getCostItem()), trueAmount, transaction);
-                    if (extracted == trueAmount) {
-                        transaction.commit();
-                        onPurchase.run();
-                    }
+                if (extract(player, getCostItem(), trueAmount)) {
+                    onPurchase.run();
                 }
             }
         }
@@ -33,7 +50,7 @@ public enum EconomyType {
     public static Text getCost() {
         return switch (HeadInitializer.headHandler.getModel().economy_type) {
             case ITEM ->
-                    Text.empty().append(getCostItem().getName()).append(Text.of(" × " + HeadInitializer.headHandler.getModel().cost_amount));
+                Text.empty().append(getCostItem().getName()).append(Text.of(" × " + HeadInitializer.headHandler.getModel().cost_amount));
             case FREE -> Text.empty();
         };
     }
