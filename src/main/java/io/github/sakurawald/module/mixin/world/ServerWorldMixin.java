58700c83b8719c266ab893bc39e8e7f8b51af864
sakurawald <sakurawald@gmail.com>
2024-09-28 15:06:45 +0800

    feature: soft fail for tickWeather(). (world module)

--- src/main/java/io/github/sakurawald/module/mixin/world/ServerWorldMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/world/ServerWorldMixin.java
@@ -1,10 +1,11 @@
 package io.github.sakurawald.module.mixin.world;
 
 import net.minecraft.network.packet.Packet;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,17 +18,19 @@ public abstract class ServerWorldMixin {
     public abstract ServerChunkManager getChunkManager();
 
     @Redirect(
-            method = "tickWeather",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/packet/Packet;)V"
-            )
-
+        method = "tickWeather",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/PlayerManager;sendToAll(Lnet/minecraft/network/packet/Packet;)V"
+        )
+        // this mixin will fail to mixin in neoforge platform.
+        , require = 0
     )
-    private void dontSendWeatherPacketsToAllWorlds(@NotNull PlayerManager instance, Packet<?> packet) {
+    private void dontSendWeatherPacketsToAllWorlds(PlayerManager instance, Packet<?> packet) {
         // Vanilla sends rain packets to all players when rain starts in a world,
         // even if they are not in it, meaning that if it is possible to rain in the world they are in
         // the rain effect will remain until the player changes dimension or reconnects.
-        instance.sendToDimension(packet, this.getChunkManager().getWorld().getRegistryKey());
+        RegistryKey<World> registryKey = this.getChunkManager().getWorld().getRegistryKey();
+        instance.sendToDimension(packet, registryKey);
     }
 }
