2351d82b411e1e93cba6ad39fd7f28d07d216077
sakurawald <sakurawald@gmail.com>
2024-09-28 12:13:29 +0800

    refactor: replace the event with mixin for command event module.

--- src/main/java/io/github/sakurawald/module/initializer/command_event/CommandEventInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_event/CommandEventInitializer.java
@@ -2,21 +2,11 @@ package io.github.sakurawald.module.initializer.command_event;
 
 import io.github.sakurawald.core.config.handler.abst.BaseConfigurationHandler;
 import io.github.sakurawald.core.config.handler.impl.ObjectConfigurationHandler;
-import io.github.sakurawald.core.service.command_executor.CommandExecutor;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import io.github.sakurawald.module.initializer.command_event.config.model.CommandEventConfigModel;
-import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 
 public class CommandEventInitializer extends ModuleInitializer {
 
     public static final BaseConfigurationHandler<CommandEventConfigModel> config = new ObjectConfigurationHandler<>(BaseConfigurationHandler.CONFIG_JSON, CommandEventConfigModel.class);
-
-    @Override
-    public void onInitialize() {
-        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> CommandExecutor.executeSpecializedCommand(newPlayer, config.getModel().event.after_player_respawn.command_list));
-
-        ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> CommandExecutor.executeSpecializedCommand(player, config.getModel().event.after_player_change_world.command_list));
-    }
 
 }
--- src/main/java/io/github/sakurawald/module/mixin/command_event/ServerPlayerEntityMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/command_event/ServerPlayerEntityMixin.java
@@ -4,6 +4,7 @@ import io.github.sakurawald.core.service.command_executor.CommandExecutor;
 import io.github.sakurawald.module.initializer.command_event.CommandEventInitializer;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,10 +13,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public class ServerPlayerEntityMixin {
 
+
     @Inject(method = "onDeath", at = @At("HEAD"))
     public void onPlayerDeath(DamageSource damageSource, CallbackInfo ci) {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
         CommandExecutor.executeSpecializedCommand(player, CommandEventInitializer.config.getModel().event.on_player_death.command_list);
     }
 
+    @Inject(method = "worldChanged(Lnet/minecraft/server/world/ServerWorld;)V", at = @At("TAIL"))
+	private void afterWorldChanged(ServerWorld origin, CallbackInfo ci) {
+        ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
+        CommandExecutor.executeSpecializedCommand(player, CommandEventInitializer.config.getModel().event.after_player_change_world.command_list);
+	}
 }
