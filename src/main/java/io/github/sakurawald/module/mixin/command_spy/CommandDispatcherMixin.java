11a7c2c935d8c14ad6b567041403f74563a6e3f3
sakurawald <sakurawald@gmail.com>
2024-09-29 16:32:25 +0800

    fix: the command spy module can't world in client.

--- src/main/java/io/github/sakurawald/module/mixin/command_spy/CommandDispatcherMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/command_spy/CommandDispatcherMixin.java
@@ -3,6 +3,7 @@ package io.github.sakurawald.module.mixin.command_spy;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.ParseResults;
 import io.github.sakurawald.module.initializer.command_spy.CommandSpyInitializer;
+import net.minecraft.client.network.ClientCommandSource;
 import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,8 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = CommandDispatcher.class, remap = false)
 public class CommandDispatcherMixin {
 
+    @SuppressWarnings("unchecked")
     @Inject(method = "execute(Lcom/mojang/brigadier/ParseResults;)I", at = @At("HEAD"))
-    public void spyOnCommandParse(ParseResults<ServerCommandSource> parse, CallbackInfoReturnable<Integer> cir) {
-        CommandSpyInitializer.process(parse);
+    public void onExecuteInCommandDispatcher(ParseResults<?> parse, CallbackInfoReturnable<Integer> cir) {
+        // in client, the S is not guarantee to be ServerCommandSource
+        if (parse.getContext().getSource() instanceof ServerCommandSource) {
+            CommandSpyInitializer.process((ParseResults<ServerCommandSource>) parse);
+        }
     }
 }
