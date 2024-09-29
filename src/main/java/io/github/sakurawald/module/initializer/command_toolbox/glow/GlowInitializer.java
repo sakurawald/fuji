78250bdeab620155a836dcfbfbb2d659f8527ac7
sakurawald <sakurawald@gmail.com>
2024-09-29 14:08:07 +0800

    feature: add `/glow` command. (glow module)

---
+++ src/main/java/io/github/sakurawald/module/initializer/command_toolbox/glow/GlowInitializer.java
@@ -0,0 +1,21 @@
+package io.github.sakurawald.module.initializer.command_toolbox.glow;
+
+import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
+import io.github.sakurawald.core.auxiliary.minecraft.LocaleHelper;
+import io.github.sakurawald.core.command.annotation.CommandNode;
+import io.github.sakurawald.core.command.annotation.CommandSource;
+import io.github.sakurawald.module.initializer.ModuleInitializer;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+
+public class GlowInitializer extends ModuleInitializer {
+
+    @CommandNode("glow")
+    private static int $glow(@CommandSource ServerPlayerEntity player) {
+        boolean flag = !player.isGlowing();
+        player.setGlowing(flag);
+        LocaleHelper.sendMessageByKey(player, flag ? "glow.on" : "glow.off");
+        return CommandHelper.Return.SUCCESS;
+    }
+
+}
