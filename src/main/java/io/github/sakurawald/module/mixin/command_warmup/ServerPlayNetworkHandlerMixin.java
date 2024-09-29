0fe78d28249f23ae24a783b15de1c2ada93c428f
sakurawald <sakurawald@gmail.com>
2024-09-29 14:48:04 +0800

    feature: improve the priority of command warmup module. (better compatibility with other mods.)

--- src/main/java/io/github/sakurawald/module/mixin/command_warmup/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/io/github/sakurawald/module/mixin/command_warmup/ServerPlayNetworkHandlerMixin.java
@@ -1,42 +1,29 @@
 package io.github.sakurawald.module.mixin.command_warmup;
 
 import io.github.sakurawald.core.manager.Managers;
-import io.github.sakurawald.module.initializer.command_warmup.structure.CommandWarmupTicket;
 import io.github.sakurawald.module.initializer.command_warmup.CommandWarmupInitializer;
+import io.github.sakurawald.module.initializer.command_warmup.structure.CommandWarmupTicket;
 import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Map;
-
-@Mixin(net.minecraft.server.network.ServerPlayNetworkHandler.class)
+@Mixin(value = ServerPlayNetworkHandler.class, priority = 1000 - 500)
 public abstract class ServerPlayNetworkHandlerMixin {
 
     @Shadow
-    public abstract ServerPlayerEntity getPlayer();
-
-    @Unique
-    public int getMs(@NotNull String commandLine) {
-        for (Map.Entry<String, Integer> entry : CommandWarmupInitializer.config.getModel().regex2ms.entrySet()) {
-            if (!commandLine.matches(entry.getKey())) continue;
-            return entry.getValue();
-        }
-
-        return 0;
-    }
+    public ServerPlayerEntity player;
 
     @Inject(method = "onCommandExecution", at = @At("HEAD"), cancellable = true)
     public void interceptCommandUsagePackets(@NotNull CommandExecutionC2SPacket commandExecutionC2SPacket, @NotNull CallbackInfo ci) {
-        ServerPlayerEntity player = getPlayer();
         String command = commandExecutionC2SPacket.comp_808();
 
-        int ms = getMs(command);
+        int ms = CommandWarmupInitializer.command2ms(command);
         if (ms > 0) {
             Managers.getBossBarManager().addTicket(CommandWarmupTicket.make(player, command, ms));
             ci.cancel();
