4403525fbb8604e72896a39518f8484a16ebe86a
sakurawald <sakurawald@gmail.com>
2024-09-29 15:43:07 +0800

    refactor: simplify the command cooldown module

--- src/main/java/io/github/sakurawald/module/initializer/command_cooldown/config/model/CommandCooldownConfigModel.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_cooldown/config/model/CommandCooldownConfigModel.java
@@ -1,14 +1,18 @@
 package io.github.sakurawald.module.initializer.command_cooldown.config.model;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CommandCooldownConfigModel {
 
-    public HashMap<String, Long> regex2ms = new HashMap<>() {
+    public Map<String, Long> regex2ms = new HashMap<>() {
         {
-            this.put("rw tp (overworld|the_nether|the_end)", 120 * 1000L);
+            this.put("world tp (overworld|the_nether|the_end)", 120 * 1000L);
             this.put("chunks\\s*", 60 * 1000L);
+            this.put("rtp\\s*", 60 * 1000L);
             this.put("download\\s*", 120 * 1000L);
+            this.put("heal\\s*", 300 * 1000L);
+            this.put("repair\\s*", 300 * 1000L);
         }
     };
 }
