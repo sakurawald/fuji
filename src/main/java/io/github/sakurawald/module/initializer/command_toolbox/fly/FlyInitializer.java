cc870dc42d4e470398bc6f7ec6de2650e011391f
sakurawald <sakurawald@gmail.com>
2024-09-29 14:13:18 +0800

    feature: now requires the permission level = 4 to use `/fly` and `/god`. (god module + fly module)

--- src/main/java/io/github/sakurawald/module/initializer/command_toolbox/fly/FlyInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_toolbox/fly/FlyInitializer.java
@@ -3,6 +3,7 @@ package io.github.sakurawald.module.initializer.command_toolbox.fly;
 import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.LocaleHelper;
 import io.github.sakurawald.core.command.annotation.CommandNode;
+import io.github.sakurawald.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -11,6 +12,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 public class FlyInitializer extends ModuleInitializer {
 
     @CommandNode("fly")
+    @CommandRequirement(level = 4)
     private static int $fly(@CommandSource ServerPlayerEntity player) {
         boolean flag = !player.getAbilities().allowFlying;
         player.getAbilities().allowFlying = flag;
--- src/main/java/io/github/sakurawald/module/initializer/command_toolbox/god/GodInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_toolbox/god/GodInitializer.java
@@ -3,6 +3,7 @@ package io.github.sakurawald.module.initializer.command_toolbox.god;
 import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.LocaleHelper;
 import io.github.sakurawald.core.command.annotation.CommandNode;
+import io.github.sakurawald.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -10,8 +11,8 @@ import net.minecraft.server.network.ServerPlayerEntity;
 
 public class GodInitializer extends ModuleInitializer {
 
-
     @CommandNode("god")
+    @CommandRequirement(level = 4)
     private static int $god(@CommandSource ServerPlayerEntity player) {
         boolean flag = !player.getAbilities().invulnerable;
         player.getAbilities().invulnerable = flag;
