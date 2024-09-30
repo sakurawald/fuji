4593e28814d3cba8451da834984687948af35627
sakurawald <sakurawald@gmail.com>
2024-09-30 15:40:57 +0800

    feature: add sign cache for `color.sign` module, make the `sign text edit` reversible, never lose the edit work!.

---
+++ src/main/java/io/github/sakurawald/module/initializer/color/sign/structure/SignCache.java
@@ -0,0 +1,10 @@
+package io.github.sakurawald.module.initializer.color.sign.structure;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SignCache {
+    final List<String> lines;
+}
