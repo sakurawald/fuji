3454cc0518d348a0aa4a8dd779932788b7415c5d
sakurawald <sakurawald@gmail.com>
2024-10-01 22:08:28 +0800

    feature: a detailed exception feedback for `/run as console`, `/run as player` and `/run as fake-player`

--- src/main/java/io/github/sakurawald/core/service/command_executor/CommandExecutor.java
+++ src/main/java/io/github/sakurawald/core/service/command_executor/CommandExecutor.java
@@ -7,10 +7,13 @@ import io.github.sakurawald.core.auxiliary.LogUtil;
 import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.LocaleHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.ServerHelper;
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -50,32 +53,37 @@ public class CommandExecutor {
 
             return ServerHelper.getCommandDispatcher().execute(command, server.getCommandSource());
         } catch (CommandSyntaxException e) {
-            LogUtil.error("CommandExecuter fails to execute commands.", e);
+            LogUtil.error("CommandExecuter fails to execute the command: /{}", command, e);
         }
         return CommandHelper.Return.FAIL;
     }
 
 
+    @SneakyThrows
     public static int executeCommandAsPlayer(@NotNull PlayerEntity player, String command, Function<ServerCommandSource, ServerCommandSource> source) {
         command = LocaleHelper.resolvePlaceholder(player, command);
 
         CommandDispatcher<ServerCommandSource> dispatcher = ServerHelper.getCommandDispatcher();
 
         ServerCommandSource serverCommandSource = source.apply(player.getCommandSource());
-
         ParseResults<ServerCommandSource> parseResults
-                = dispatcher.parse(command, serverCommandSource);
+            = dispatcher.parse(command, serverCommandSource);
+
         try {
             return dispatcher.execute(parseResults);
         } catch (CommandSyntaxException e) {
-            LogUtil.error("CommandExecuter fails to execute commands.", e);
+            player.sendMessage(Text
+                .literal("During the execution of: /" + command)
+                .append(LocaleHelper.TEXT_NEWLINE)
+                .append(Text.literal("Exception: " + e.getMessage()))
+                .formatted(Formatting.RED));
         }
 
         return CommandHelper.Return.FAIL;
     }
 
     public static int executeCommandAsPlayer(@NotNull PlayerEntity player, String command) {
-       return executeCommandAsPlayer(player, command, (source) -> source);
+        return executeCommandAsPlayer(player, command, (source) -> source);
     }
 
     public static int executeCommandAsFakeOp(@NotNull PlayerEntity player, String command) {
