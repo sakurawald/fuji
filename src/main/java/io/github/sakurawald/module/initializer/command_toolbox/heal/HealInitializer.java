0ff4e576267ce7a34d8a3c6ceae65dcea1312226
sakurawald <sakurawald@gmail.com>
2024-10-03 14:45:25 +0800

    feature: add optional target player argument for `/heal` and `/feed`

--- src/main/java/io/github/sakurawald/module/initializer/command_toolbox/heal/HealInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_toolbox/heal/HealInitializer.java
@@ -3,6 +3,7 @@ package io.github.sakurawald.module.initializer.command_toolbox.heal;
 import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.LocaleHelper;
 import io.github.sakurawald.core.command.annotation.CommandNode;
+import io.github.sakurawald.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -13,6 +14,12 @@ public class HealInitializer extends ModuleInitializer {
 
     @CommandNode("heal")
     private static int $heal(@CommandSource ServerPlayerEntity player) {
+        return $heal(player, player);
+    }
+
+    @CommandNode("heal")
+    @CommandRequirement(level = 4)
+    private static int $heal(@CommandSource ServerPlayerEntity player, ServerPlayerEntity target) {
         player.setHealth(player.getMaxHealth());
         LocaleHelper.sendMessageByKey(player, "heal");
         return CommandHelper.Return.SUCCESS;
