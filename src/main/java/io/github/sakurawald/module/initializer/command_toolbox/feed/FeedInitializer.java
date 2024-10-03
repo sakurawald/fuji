2817fb1f5b392197a6e7219b785a773847bd919c
sakurawald <sakurawald@gmail.com>
2024-10-03 14:45:25 +0800

    feature: add optional target player argument for `/heal` and `/feed`

--- src/main/java/io/github/sakurawald/module/initializer/command_toolbox/feed/FeedInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_toolbox/feed/FeedInitializer.java
@@ -3,6 +3,7 @@ package io.github.sakurawald.module.initializer.command_toolbox.feed;
 import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.LocaleHelper;
 import io.github.sakurawald.core.command.annotation.CommandNode;
+import io.github.sakurawald.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import net.minecraft.entity.player.HungerManager;
@@ -13,6 +14,12 @@ public class FeedInitializer extends ModuleInitializer {
 
     @CommandNode("feed")
     private static int $feed(@CommandSource ServerPlayerEntity player) {
+        return $feed(player, player);
+    }
+
+    @CommandNode("feed")
+    @CommandRequirement(level = 4)
+    private static int $feed(@CommandSource ServerPlayerEntity player, ServerPlayerEntity target) {
         HungerManager foodData = player.getHungerManager();
         foodData.setFoodLevel(20);
         foodData.setSaturationLevel(5);
@@ -21,5 +28,4 @@ public class FeedInitializer extends ModuleInitializer {
         LocaleHelper.sendMessageByKey(player, "feed");
         return CommandHelper.Return.SUCCESS;
     }
-
 }
