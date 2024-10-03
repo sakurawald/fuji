6243dc04f93d3e029540abb329c7bb8978f5112a
sakurawald <sakurawald@gmail.com>
2024-10-03 13:30:55 +0800

    refactor: pull the regex into Command class.

--- src/main/java/io/github/sakurawald/module/initializer/command_warmup/config/model/CommandWarmupConfigModel.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_warmup/config/model/CommandWarmupConfigModel.java
@@ -10,7 +10,8 @@ public class CommandWarmupConfigModel {
 
     public List<CommandWarmupEntry> entries = new ArrayList<>(){
         {
-            this.add(new CommandWarmupEntry("back", 3 * 1000, new Interruptible(true, 3, true, true)));
+            this.add(new CommandWarmupEntry(new CommandWarmupEntry.Command("back", 3 * 1000), new Interruptible(true, 3, true, true)));
+            this.add(new CommandWarmupEntry(new CommandWarmupEntry.Command("heal", 1000), new Interruptible(true, 3, true, true)));
         }
     };
 }
