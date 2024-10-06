9326a32ba6aa1cc2b77d3ef421c530fa3393bf71
sakurawald <sakurawald@gmail.com>
2024-10-06 19:57:12 +0800

    refactor: set the default requirement of commands from predicate module to level 4, since these commands is useless for default player.

--- src/main/java/io/github/sakurawald/module/initializer/predicate/PredicateInitializer.java
+++ src/main/java/io/github/sakurawald/module/initializer/predicate/PredicateInitializer.java
@@ -3,12 +3,14 @@ package io.github.sakurawald.module.initializer.predicate;
 import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.PermissionHelper;
 import io.github.sakurawald.core.command.annotation.CommandNode;
+import io.github.sakurawald.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.core.command.argument.wrapper.impl.GreedyString;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+@CommandRequirement(level = 4)
 public class PredicateInitializer extends ModuleInitializer {
 
     @CommandNode("test-string-perm")
