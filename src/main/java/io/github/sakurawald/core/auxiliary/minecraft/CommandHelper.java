41eb318c3ba37c6ca279149ad794b85a4b5b2683
sakurawald <sakurawald@gmail.com>
2024-10-08 08:08:07 +0800

    feature: only send the message feedback to player command source, to avoid console spam. (predicate module)

--- src/main/java/io/github/sakurawald/core/auxiliary/minecraft/CommandHelper.java
+++ src/main/java/io/github/sakurawald/core/auxiliary/minecraft/CommandHelper.java
@@ -68,7 +68,10 @@ public class CommandHelper {
         }
 
         public static int outputBoolean(ServerCommandSource source, boolean value) {
-            source.sendMessage(Text.literal(String.valueOf(value)));
+            // only send the message feedback to player, to avoid the console spam
+            if (source.isExecutedByPlayer()) {
+                source.sendMessage(Text.literal(String.valueOf(value)));
+            }
             return fromBoolean(value);
         }
     }
