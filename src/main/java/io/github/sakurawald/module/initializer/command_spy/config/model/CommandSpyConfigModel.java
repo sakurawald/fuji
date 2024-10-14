68a86178650f005cc6c2b4b456a85f3d119d7b4e
sakurawald <sakurawald@gmail.com>
2024-10-15 01:53:53 +0800

    feature: add `ignore` option. (command spy module)

--- src/main/java/io/github/sakurawald/module/initializer/command_spy/config/model/CommandSpyConfigModel.java
+++ src/main/java/io/github/sakurawald/module/initializer/command_spy/config/model/CommandSpyConfigModel.java
@@ -1,5 +1,15 @@
 package io.github.sakurawald.module.initializer.command_spy.config.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandSpyConfigModel {
+
+    public List<String> ignore = new ArrayList<>() {
+        {
+            this.add("login.*");
+        }
+    };
+
     public boolean spy_on_console = false;
 }
