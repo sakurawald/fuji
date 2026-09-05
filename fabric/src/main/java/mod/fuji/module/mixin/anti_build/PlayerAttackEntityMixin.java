26e39c1753de42b37d1a62bbabea2a62bec76910
sakurawald <sakurawald@gmail.com>
2026-09-05 17:21:53 +0800

    fix: should ignore the client player for attack entity in anti build module.

--- fabric/src/main/java/mod/fuji/module/mixin/anti_build/PlayerAttackEntityMixin.java
+++ fabric/src/main/java/mod/fuji/module/mixin/anti_build/PlayerAttackEntityMixin.java
@@ -1,16 +1,17 @@
 package mod.fuji.module.mixin.anti_build;
 
+import mod.fuji.core.auxiliary.minecraft.PlayerHelper;
 import mod.fuji.core.auxiliary.minecraft.RegistryHelper;
 import mod.fuji.module.initializer.anti_build.AntiBuildInitializer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
 
 #if MC_VER < MC_26_1
+import net.minecraft.server.level.ServerPlayer;
 @Mixin(ServerPlayer.class)
 #elif MC_VER >= MC_26_1
 @Mixin(Player.class)
@@ -19,12 +20,13 @@ public class PlayerAttackEntityMixin {
 
     @Inject(method = "attack", at = @At("HEAD"), cancellable = true)
     void handleAttackEntity(Entity entity, CallbackInfo ci) {
-        var config = AntiBuildInitializer.config.model().getAntiActionTypes().getAttackEntity();
-        if (!config.isEnable()) return;
+        PlayerHelper.Kind.ifServerPlayerEntity(this, player -> {
+            var config = AntiBuildInitializer.config.model().getAntiActionTypes().getAttackEntity();
+            if (!config.isEnable()) return;
 
-        ServerPlayer serverPlayerEntity = (ServerPlayer) (Object) this;
-        String id = RegistryHelper.getIdAsString(entity);
-        AntiBuildInitializer.processAntiBuildAction(serverPlayerEntity, "attack_entity", config.getId(), id, ci::cancel, () -> true);
+            String id = RegistryHelper.getIdAsString(entity);
+            AntiBuildInitializer.processAntiBuildAction(player, "attack_entity", config.getId(), id, ci::cancel, () -> true);
+        });
     }
 
 }
